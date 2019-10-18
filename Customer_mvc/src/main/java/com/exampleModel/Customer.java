b0707697035f0fc16c081f247ba53bacb55e970b
Akshathakokkarne <kokkarneakshatha@gmail.com>
2019-10-18 08:37:57 +0530

    Customer_mvc

---
+++ Customer_mvc/src/main/java/com/exampleModel/Customer.java
@@ -0,0 +1,17 @@
+package com.exampleModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Customer {
+	private String name=null;
+	private int Custid=-1;
+	private String type=null;
+
+}
